a343fda1e64c9bd71828a9350da171ffe5dcb040
Rohit Rawat <rohitrawat@gmail.com>
2015-01-25 01:13:36 -0600

    Added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion functions with caching.
 
-## Write a short comment describing this function
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+## This function creates a CacheMatrix object from a matrix. The CacheMatrix 
+## object is iverted using the cacheSolve() function. The inversion result is 
+## cached inside the object when it is first computed. The cached result is 
+## returned on subsequent inversion.
+## Usage:
+## x <- matrix(rnorm(5*5),5,5)
+## cx <- makeCacheMatrix(x)
+## print(cx$get())
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -11,6 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
         inv <<- NULL
     }
     
+    # access the underlying matrix object
     get <- function() {
         x
     }
@@ -25,9 +33,13 @@ makeCacheMatrix <- function(x = matrix()) {
     
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
 
-
-## Write a short comment describing this function
+## This function computes the inverse of a CacheMatrix object. The inverse is 
+## stored inside the object and is retreived on subsequent calls to the same
+## object.
+## Usage:
+## xinv = cacheSolve(cx)
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
